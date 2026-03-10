0b06197325c0692f0cb0e9e5a5facfe2f36c3f3e
PRAVEENAT123 <praveenm@yectra.com>
2026-03-10 15:18:44 +0530

    Impliment dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import Logo from "../assets/react.svg";
+import { ColorModeButton } from "./ui/color-mode";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={Logo}></Image>
       <Text> Navbar</Text>
+      <ColorModeButton></ColorModeButton>
     </HStack>
   );
 };
