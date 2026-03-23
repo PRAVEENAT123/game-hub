47dbb7f5efdb0c4da73e3d582322a3a730c3ed59
Praveen270501 <praveen.270501@gmail.com>
2026-03-23 12:17:29 +0530

    Testing code

--- src/components/GameCart.tsx
+++ src/components/GameCart.tsx
@@ -8,7 +8,7 @@ import getCroppedImageURL from "@/services/iamge-url";
 interface Props {
   game: Game;
 }
-// Added the comment
+// Added the comment code
 const GameCart = ({ game }: Props) => {
   return (
     <Card.Root overflow="hidden" borderRadius={10}>
